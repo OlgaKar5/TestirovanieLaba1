c64d9bde94bca8766dba7da789f83a99de3acfa5
Карасева <karkar00q@gmail.com>
2026-03-12 10:02:33 +0300

    2. Добавление новых классов Note, NoteManager

---
+++ ManagerNotes/ManagerNotes/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ManagerNotes
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+
+        public Note(string title, string content)
+        {
+            Title = title;
+            Content = content;
+            Date = DateTime.Now;
+        }
+    }
+}
